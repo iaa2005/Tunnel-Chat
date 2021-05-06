fe6cf6188ec5a2e41871bc6d87e2994a6456e97c
iaa2005 <marsee3015@gmail.com>
2021-05-06 14:40:40 +0300

    -

--- main.py
+++ main.py
@@ -26,6 +26,10 @@ elif platform == "darwin": # OS X
         "labelMadeBy-fontsize": 12,
         "labelMadeBy-x": 10,
         "labelMadeBy-y": 37,
+        "chooseFile-x": 206,
+        "chooseFile-y": 615,
+        "sendFileBtn-x": 300,
+        "sendFileBtn-y": 615,
     }
 elif platform == "win32": # Windows
     design = {
@@ -34,6 +38,10 @@ elif platform == "win32": # Windows
         "labelMadeBy-fontsize": 10,
         "labelMadeBy-x": 12,
         "labelMadeBy-y": 40,
+        "chooseFile-x": 220,
+        "chooseFile-y": 615,
+        "sendFileBtn-x": 305,
+        "sendFileBtn-y": 615,
     }
 
 DARK_GREY = "#171717"
@@ -55,7 +63,7 @@ def send_msg(msg):
 
 
 window = tk.Tk()
-window.geometry("400x650")
+window.geometry("400x670")
 window.configure(bg=DARK_GREY)
 window.title("Tunnel Chat v4.0")
 window.resizable(height=False, width=False)
@@ -67,6 +75,9 @@ labelTunnel.place(x=10, y=10)
 labelMadeBy = tk.Label(text="Made by @iaa2005", bg=DARK_GREY, fg=WHITE_03,
                        font=tkFont.Font(family="Helvetica", size=design["labelMadeBy-fontsize"], weight="normal"))
 labelMadeBy.place(x=design["labelMadeBy-x"], y=design["labelMadeBy-y"])
+
+genBtn = tk.Button(text="Info", width=6, bd=0)
+genBtn.place(x=325, y=137)
 
 Copied = tk.Label(text="Copied", bg=DARK_GREY, fg=DARK_GREY,
                   font=tkFont.Font(family="Helvetica", size=12, weight="normal"))
@@ -95,8 +106,10 @@ labelTunnel = tk.Label(text="Or enter ChatID", bg=DARK_GREY, fg=WHITE_02,
                        font=tkFont.Font(family="Helvetica", size=16, weight="normal"))
 labelTunnel.place(x=10, y=105)
 
-EntryChatID = tk.Entry(bd=0, highlightthickness=2, highlightbackground="white")
+EntryChatID = tk.Entry(bd=0, highlightthickness=2, highlightbackground="white", width=21)
 EntryChatID.place(x=15, y=137)
+
+connectBtn = tk.Button(text="Connect", width=10)
 
 def connect():
     global CHAT_ID
@@ -104,12 +117,13 @@ def connect():
         CHAT_ID = EntryChatID.get()
         ChatIDText.configure(text=CHAT_ID)
     input_user.set("")
+    connectBtn.configure(text="Connected")
 
-connectBtn = tk.Button(text="Connect", width=10, command=connect)
+connectBtn.configure(command=connect)
 connectBtn.place(x=220, y=137)
 
 input_user = tk.StringVar()
-input_field = tk.Entry(text=input_user, highlightthickness=2, highlightbackground="white", bd=0, width=32)
+input_field = tk.Entry(text=input_user, highlightthickness=2, highlightbackground="white", bd=0, width=30)
 input_field.place(x=15, y=587)
 
 textCons = tk.Text(window, width=design["textCons-width"], height=design["textCons-height"], bd=0, wrap="word", spacing1=5, padx=15,
@@ -123,36 +137,40 @@ scrollbar.place(relheight=1, relx=1.01)
 scrollbar.config(command=textCons.yview)
 textCons.config(state=tk.DISABLED, yscrollcommand=scrollbar.set)
 
+fileText = tk.Label(text="", bg=DARK_GREY, fg=WHITE_03,
+                       font=tkFont.Font(family="Helvetica", size=10, weight="normal"))
+fileText.place(x=10, y=615)
 
 def get_history_chat():
-    while CHAT_ID == "":
-        pass
-    if CHAT_ID != "":
-        history = dweepy.get_dweets_for(CHAT_ID)
-        for msg in history:
-            try:
-                date = msg["created"][12:17]
-                type = msg["content"]["type"]
-                username = msg["content"]["username"]
-                if type == "file":
-                    for index in int(msg["content"]["data"]["count"]):
-                        filename = msg["content"]["data"][str(index)]["name"]
-                        text_cons = date + " " + username + ": sent file " + filename
-                        textCons.config(state=tk.NORMAL)
-                        textCons.insert(tk.END, text_cons + "\n")
-                        textCons.config(state=tk.DISABLED)
-                        textCons.see(tk.END)
-                elif type == "text":
-                    data = msg["content"]["data"]
-                    data = str(data)[1:]
-                    data = bytes.fromhex(data).decode("utf-8")
-                    text_cons = date + " " + username + ": " + data
-                    textCons.config(state=tk.NORMAL)
-                    textCons.insert(tk.END, text_cons + "\n")
-                    textCons.config(state=tk.DISABLED)
-                    textCons.see(tk.END)
-            except:
-                pass
+    pass
+    # while CHAT_ID == "":
+    #     pass
+    # if CHAT_ID != "":
+    #     history = dweepy.get_dweets_for(CHAT_ID)
+    #     for msg in history:
+    #         try:
+    #             date = msg["created"][12:17]
+    #             type = msg["content"]["type"]
+    #             username = msg["content"]["username"]
+    #             if type == "file":
+    #                 for index in int(msg["content"]["data"]["count"]):
+    #                     filename = msg["content"]["data"][str(index)]["name"]
+    #                     text_cons = date + " " + username + ": sent file " + filename
+    #                     textCons.config(state=tk.NORMAL)
+    #                     textCons.insert(tk.END, text_cons + "\n")
+    #                     textCons.config(state=tk.DISABLED)
+    #                     textCons.see(tk.END)
+    #             elif type == "text":
+    #                 data = msg["content"]["data"]
+    #                 data = str(data)[1:]
+    #                 data = bytes.fromhex(data).decode("utf-8")
+    #                 text_cons = date + " " + username + ": " + data
+    #                 textCons.config(state=tk.NORMAL)
+    #                 textCons.insert(tk.END, text_cons + "\n")
+    #                 textCons.config(state=tk.DISABLED)
+    #                 textCons.see(tk.END)
+    #         except:
+    #             pass
 
 
 def send_msg(input_get):
@@ -174,6 +192,7 @@ def send_files():
         dweepy.dweet_for(CHAT_ID, {"username": USERNAME, "type": "file", "data": FILES})
 
 def choose_file():
+    filenames_window = ""
     filenames = filedialog.askopenfilenames(initialdir="/", title="Select a file",
                                             # filetype=(("All Files", "*.*"), ("jpeg", "*.jpeg"))
                                             )
@@ -183,6 +202,7 @@ def choose_file():
         count += 1
         name, ext = os.path.splitext(file_name)
         name = ntpath.basename(file_name)
+        filenames_window += name + "\n"
         text_type_files[str(count)] = {
             "name": "x" + binascii.hexlify(str(name).encode("utf-8")).decode("utf-8"),
             "ext": "x" + binascii.hexlify(str(ext[1:]).encode("utf-8")).decode("utf-8"),
@@ -195,6 +215,7 @@ def choose_file():
         global CAN_SEND_FILE, FILES
         FILES = text_type_files
         CAN_SEND_FILE = True
+        fileText.configure(text=filenames_window)
         # print filenames in program
     else:
         pass
@@ -203,14 +224,14 @@ def choose_file():
     # pprint.pprint(text_type_files)
 
 
-sendBtn = tk.Button(text="Send", width=6, command=send_text, bd=0)
-sendBtn.place(x=325, y=587)
+sendBtn = tk.Button(text="Send", width=9, command=send_text, bd=0)
+sendBtn.place(x=300, y=587)
 
-chooseFile = tk.Button(text="Choose file", width=10, command=choose_file, bd=0)
-chooseFile.place(x=220, y=615)
+chooseFile = tk.Button(text="Choose file", width=9, command=choose_file, bd=0)
+chooseFile.place(x=design["chooseFile-x"], y=design["chooseFile-y"])
 
-sendFileBtn = tk.Button(text="Send file", width=10, command=send_files, bd=0)
-sendFileBtn.place(x=305, y=615)
+sendFileBtn = tk.Button(text="Send file", width=9, command=send_files, bd=0)
+sendFileBtn.place(x=design["sendFileBtn-x"], y=design["sendFileBtn-y"])
 
 
 def loop():
